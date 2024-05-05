12b3b9c94308af8d49a986e17c2c9e7acd62ce38
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-05-05 20:22:37 +0100

    Add insert statements for Friends database

---
+++ FriendsApiDb/insert/insert_characters.sql
@@ -0,0 +1,93 @@
+INSERT INTO
+    characters (
+        character_id,
+        first_name,
+        last_name,
+        actor_name,
+        character_bio
+    )
+VALUES
+    (
+        '178d8748-9982-4371-b038-ebbc0a5adc97',
+        'Rachel',
+        'Green',
+        'Jennifer Aniston',
+        'Rachel Green starts off as a spoiled rich girl who flees her wedding and seeks refuge at Central Perk, where she reconnects with her childhood friend Monica. Throughout the series, she evolves into an independent and successful fashion executive.'
+    ),
+    (
+        '3ecaf0fa-5d64-4e3d-8acf-011854b4b946',
+        'Monica',
+        'Geller',
+        'Courteney Cox',
+        'Monica Geller is a perfectionist with a competitive streak, often striving to be the best in everything she does. She''s known for her meticulous cleaning habits and her role as the group''s unofficial mother figure.'
+    ),
+    (
+        'a9b185a3-7ac4-4a47-8116-b9ee0dd6b6d6',
+        'Ross',
+        'Geller',
+        'David Schwimmer',
+        'Ross Geller is a paleontologist with a passion for dinosaurs. He''s known for his on-again, off-again relationship with Rachel and his quirky sense of humor. Despite his nerdy tendencies, he''s a devoted brother and friend.'
+    ),
+    (
+        '5a34649c-4aef-49bf-84f4-d50a9e31c168',
+        'Chandler',
+        'Bing',
+        'Matthew Perry',
+        'Chandler Bing is known for his sarcastic wit and fear of commitment. He works in statistical analysis and data reconfiguration but changes careers multiple times throughout the series. Despite his flaws, he''s a loyal friend and eventually finds love with Monica.'
+    ),
+    (
+        '31996a1b-8feb-45ae-9768-784c902aacde',
+        'Joey',
+        'Tribbiani',
+        'Matt LeBlanc',
+        'Joey Tribbiani is an aspiring actor with a penchant for food and women. He''s known for his catchphrase "How you doin''?" and his lovable but dim-witted personality. Despite his struggles with acting, he remains optimistic and loyal to his friends.'
+    ),
+    (
+        'f69cfe71-bf5c-4607-9bf3-a7c4220cf747',
+        'Phoebe',
+        'Buffay',
+        'Lisa Kudrow',
+        'Phoebe Buffay is a quirky and eccentric musician with a traumatic past. She''s known for her unusual beliefs, offbeat humor, and unique songs, such as "Smelly Cat." Despite her unconventional upbringing, she''s incredibly kind-hearted and supportive of her friends.'
+    ),
+    (
+        'cfe98775-346a-4015-8c99-e6f9bdad687f',
+        'Gunther',
+        '',
+        'James Michael Tyler',
+        'Gunther is the manager of Central Perk, where the friends frequently hang out. He harbors a secret crush on Rachel and often displays jealousy towards her romantic interests. Despite his minimal interaction with the group, he becomes an integral part of the show''s ensemble.'
+    ),
+    (
+        'c4559501-995c-4d45-8796-b977b726b37c',
+        'Janice',
+        'Litman-Goralnik',
+        'Maggie Wheeler',
+        'Janice is known for her distinct nasal voice and iconic catchphrase "Oh my God!" She''s Chandler''s on-again, off-again girlfriend throughout the series and often pops up unexpectedly in the friends'' lives, much to their chagrin.'
+    ),
+    (
+        '8c290b66-8a21-40c8-a055-36b01b4821c7',
+        'Richard',
+        'Burke',
+        'Tom Selleck',
+        'Richard Burke is a handsome, older ophthalmologist who dates Monica in the later seasons of the show. Despite their significant age difference, he forms a deep connection with Monica, but their relationship ultimately ends due to their differing desires for marriage and children.'
+    ),
+    (
+        'd835a2d9-c359-49d5-8a14-918c0acb6f0c',
+        'Judy',
+        'Geller',
+        'Christina Pickles',
+        'Judy Geller is the mother of Ross and Monica. She''s known for her critical and overbearing nature, often meddling in her children''s lives and making passive-aggressive remarks. Despite her flaws, she loves her children deeply and wants what''s best for them.'
+    ),
+    (
+        '95667828-ad2f-4c95-bc8b-ff39954afc54',
+        'Jack',
+        'Geller',
+        'Elliott Gould',
+        'Jack Geller is the father of Ross and Monica. He''s known for his jovial personality and tendency to embarrass his children with his inappropriate jokes and stories. Despite his quirks, he''s a loving and supportive father who wants his children to be happy.'
+    ),
+    (
+        '823fda5a-0d1c-4f5c-bb82-d29b6399ce74',
+        'Mike',
+        'Hannigan',
+        'Paul Rudd',
+        'Mike Hannigan is a sweet-natured and quirky pianist who becomes Phoebe''s love interest in the later seasons of the show. Despite their initial differences, he forms a strong bond with Phoebe and eventually marries her in a whimsical outdoor ceremony.'
+    );
---
+++ FriendsApiDb/insert/insert_quotes.sql
@@ -0,0 +1,219 @@
+INSERT INTO
+    quotes (quote_id, quote_text, character_id, episode_id)
+VALUES
+    (
+        '6f51eb95-dac9-4799-b7ed-b4b561bfa2c3',
+        'I got off the plane.',
+        '178d8748-9982-4371-b038-ebbc0a5adc97',
+        NULL
+    ),
+    (
+        '5fd4ca8b-349b-475e-8aeb-8d1e88d726dd',
+        'No, it''s not that common, it doesn''t happen to every guy, and it IS a big deal!',
+        '178d8748-9982-4371-b038-ebbc0a5adc97',
+        NULL
+    ),
+    (
+        '77e0dffd-e37a-4f15-9062-e55b5885fee4',
+        'It''s like all my life, everyone''s always told me, "You''re a shoe! You''re a shoe! You''re a shoe!" Well, what if I don''t want to be a shoe? What if I want to be a purse, you know, or a hat!',
+        '178d8748-9982-4371-b038-ebbc0a5adc97',
+        NULL
+    ),
+    (
+        '73c50e7e-106f-42c1-81e7-462e6af6ee6a',
+        'Welcome to the real world! It sucks. You''re gonna love it.',
+        '3ecaf0fa-5d64-4e3d-8acf-011854b4b946',
+        NULL
+    ),
+    (
+        '1f67259e-2a52-40e0-b3ff-8f1a1755e078',
+        'Seven! Seven! Seven!',
+        '3ecaf0fa-5d64-4e3d-8acf-011854b4b946',
+        NULL
+    ),
+    (
+        'f6966e34-8387-4ff0-b791-087cf18f031a',
+        'I KNOW!',
+        '3ecaf0fa-5d64-4e3d-8acf-011854b4b946',
+        NULL
+    ),
+    (
+        '3de6e013-525f-4c50-90ef-3ce582b26f9d',
+        'We were on a break!',
+        'a9b185a3-7ac4-4a47-8116-b9ee0dd6b6d6',
+        NULL
+    ),
+    (
+        '99c5a2bc-f350-4af1-ba26-1f06aced0a40',
+        'Pivot! Pivot! Pivot!',
+        'a9b185a3-7ac4-4a47-8116-b9ee0dd6b6d6',
+        NULL
+    ),
+    (
+        'bd64afc6-5259-49f9-8e7e-d737255ef7ce',
+        'Unagi.',
+        'a9b185a3-7ac4-4a47-8116-b9ee0dd6b6d6',
+        NULL
+    ),
+    (
+        '6d97b411-2310-4b16-9832-0a336d691b9d',
+        'Could I BE wearing any more clothes?',
+        '5a34649c-4aef-49bf-84f4-d50a9e31c168',
+        NULL
+    ),
+    (
+        '05d04ea1-d72e-4909-93ea-78173584ff5d',
+        'Could I BE more sorry?',
+        '5a34649c-4aef-49bf-84f4-d50a9e31c168',
+        NULL
+    ),
+    (
+        'e19635c7-aaf9-40d7-9c1d-e2ccb600b241',
+        'Why don''t you just get a whole human skin rug?',
+        '5a34649c-4aef-49bf-84f4-d50a9e31c168',
+        NULL
+    ),
+    (
+        '321d8f00-260c-4f32-8910-310a9a5b320b',
+        'How you doin''?',
+        '31996a1b-8feb-45ae-9768-784c902aacde',
+        NULL
+    ),
+    (
+        'a71fa679-86f2-491a-ac4a-a7f9ec4c09bf',
+        'Joey doesn''t share food!',
+        '31996a1b-8feb-45ae-9768-784c902aacde',
+        NULL
+    ),
+    (
+        'b201ab1f-be8c-46ca-ad71-3b08ef783d2f',
+        'Joey doesn''t share food!',
+        '31996a1b-8feb-45ae-9768-784c902aacde',
+        NULL
+    ),
+    (
+        'c4d3905f-735f-4115-a2e4-14cc4a50f329',
+        'They don''t know that we know they know we know.',
+        'f69cfe71-bf5c-4607-9bf3-a7c4220cf747',
+        NULL
+    ),
+    (
+        'cebdfcbc-ee7e-4642-b342-f6139babaccc',
+        'It''s not that common, it doesn''t happen to every guy, and it IS a big deal!',
+        'f69cfe71-bf5c-4607-9bf3-a7c4220cf747',
+        NULL
+    ),
+    (
+        'ab124f11-c7a8-4392-a093-b57c4fd35e61',
+        'Smelly cat, smelly cat, what are they feeding you?',
+        'f69cfe71-bf5c-4607-9bf3-a7c4220cf747',
+        NULL
+    ),
+    (
+        '74f5bf19-cdd7-4b9b-9983-b348321b640a',
+        'What''s a synonym for “foolish”?',
+        'cfe98775-346a-4015-8c99-e6f9bdad687f',
+        NULL
+    ),
+    (
+        '9e3c75ab-628b-48a8-a45e-d91ebb9f8a77',
+        'She''s got a HUMONGOUS engagement ring!',
+        'cfe98775-346a-4015-8c99-e6f9bdad687f',
+        NULL
+    ),
+    (
+        'da311dc2-a1a6-401f-8236-a2819cb9584c',
+        'You know, on second thought, gum would be perfection.',
+        'cfe98775-346a-4015-8c99-e6f9bdad687f',
+        NULL
+    ),
+    (
+        '81a8d3e0-5824-45e3-8d42-ce45d1c8bccc',
+        'Oh. My. God!',
+        'c4559501-995c-4d45-8796-b977b726b37c',
+        NULL
+    ),
+    (
+        '3a447869-0db9-4073-8c05-03834fdea776',
+        'I DECLARE IT THE SUMMER OF GEORGE!',
+        'c4559501-995c-4d45-8796-b977b726b37c',
+        NULL
+    ),
+    (
+        'c3df0935-b3ef-4a5c-9dcd-4eaa72e470d0',
+        'Janice lit it! Janice lit it! Janice lit it!',
+        'c4559501-995c-4d45-8796-b977b726b37c',
+        NULL
+    ),
+    (
+        '4acc3f66-c75b-42f7-9dfb-45ac92b35809',
+        'Hey, you two should stay together. You deserve each other!',
+        '8c290b66-8a21-40c8-a055-36b01b4821c7',
+        NULL
+    ),
+    (
+        '7548915d-99d0-4d7e-bf02-5d909aab3b3d',
+        'I want to be the one who takes you home tonight.',
+        '8c290b66-8a21-40c8-a055-36b01b4821c7',
+        NULL
+    ),
+    (
+        'd1275ac2-df80-4102-a402-efb71da64a19',
+        'I''m gonna love you like nobody''s loved you, come rain or come shine.',
+        '8c290b66-8a21-40c8-a055-36b01b4821c7',
+        NULL
+    ),
+    (
+        '7f80fe9d-11d8-4a34-8070-7089b9d54ebd',
+        'Well, it''s always been my experience that when two people are meant to be together, eventually they''ll find their way back.',
+        'd835a2d9-c359-49d5-8a14-918c0acb6f0c',
+        NULL
+    ),
+    (
+        '22d28009-e871-4a16-b5b3-9b26f39ebb4c',
+        'It''s not your parents'' fault, they''re both morons.',
+        'd835a2d9-c359-49d5-8a14-918c0acb6f0c',
+        NULL
+    ),
+    (
+        'ba196b61-f248-417b-9cbe-857fcf947c79',
+        'Your mother''s going through a change.',
+        'd835a2d9-c359-49d5-8a14-918c0acb6f0c',
+        NULL
+    ),
+    (
+        '33e2ce17-926d-41ff-bc3f-d14cb76269e9',
+        'It''s just so hard, y''know? One day you''re young and beautiful, and the next you''re just … ''',
+        '95667828-ad2f-4c95-bc8b-ff39954afc54',
+        NULL
+    ),
+    (
+        '60f15452-fe1e-4fc5-93b5-3a81ace8a991',
+        'You know, I didn''t come over here to be insulted.',
+        '95667828-ad2f-4c95-bc8b-ff39954afc54',
+        NULL
+    ),
+    (
+        '1a949b70-334e-4f8c-b29f-4ab3ebf78de0',
+        'So Ross, you gonna ask her out?',
+        '95667828-ad2f-4c95-bc8b-ff39954afc54',
+        NULL
+    ),
+    (
+        'd7ed431d-dc57-47ca-9afc-9322c951e4fc',
+        'Is this what you were looking for?',
+        '823fda5a-0d1c-4f5c-bb82-d29b6399ce74',
+        NULL
+    ),
+    (
+        '53d241b4-381c-4f19-be7e-f1f4e1d02464',
+        'Do you realize we almost made it ten minutes without discussing our future wedding?',
+        '823fda5a-0d1c-4f5c-bb82-d29b6399ce74',
+        NULL
+    ),
+    (
+        '8a1d29e3-f6a6-4381-bd90-057fb3b14ea7',
+        'That''s right, because love is patient. It''s also blind, deaf, and toothless.',
+        '823fda5a-0d1c-4f5c-bb82-d29b6399ce74',
+        NULL
+    );
