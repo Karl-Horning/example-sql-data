5e6985012093bc03c4847930378567830702b647
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-05-05 14:20:49 +0100

    Add files to insert data into tables

---
+++ VictorianAuthors/insert/insert_authors.sql
@@ -0,0 +1,179 @@
+-- Jane Austen
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Jane',
+        'Austen',
+        '1775-12-16',
+        '1817-07-18',
+        'Jane Austen was a Georgian and Regency-era novelist born on 16 December 1775 and died on 18 July 1817. Known for her wit and keen observations, Austen''s novels explore the relationships and manners of the English landed gentry in the early 19th century.'
+    );
+
+-- Charlotte Brontë
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Charlotte',
+        'Brontë',
+        '1816-04-21',
+        '1855-03-31',
+        'Charlotte Brontë was a Victorian novelist born on 21 April 1816 and died on 31 March 1855. She is best known for her novel ''Jane Eyre,'' which remains a classic of English literature.'
+    );
+
+-- Anne Brontë
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Anne',
+        'Brontë',
+        '1820-01-17',
+        '1849-05-28',
+        'Anne Brontë was a Victorian novelist born on 17 January 1820 and died on 28 May 1849. She is known for her novels ''Agnes Grey'' and ''The Tenant of Wildfell Hall.'''
+    );
+
+-- Charles Dickens
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Charles',
+        'Dickens',
+        '1812-02-07',
+        '1870-06-09',
+        'Charles Dickens was a Victorian novelist born on 7 February 1812 and died on 9 June 1870. Dickens is one of the most celebrated and prolific authors of the 19th century. His novels, known for their vivid characters and social commentary, reflect the realities of Victorian society. Some of his most famous works include ''A Tale of Two Cities,'' ''Great Expectations,'' and ''A Christmas Carol.'''
+    );
+
+-- George Eliot
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'George',
+        'Eliot',
+        '1819-11-22',
+        '1880-12-22',
+        'George Eliot (Mary Ann Evans) was a prominent Victorian novelist born on 22 November 1819 and died on 22 December 1880. Her novels often explore complex characters, psychological insight, and societal issues of her time. ''Middlemarch'' is considered one of her masterpieces.'
+    );
+
+-- Thomas Hardy
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Thomas',
+        'Hardy',
+        '1840-06-02',
+        '1928-01-11',
+        'Thomas Hardy was a Victorian novelist born on 2 June 1840 and died on 11 January 1928. He is known for his novels that explore rural life, social issues, and the complexities of human relationships in Victorian England.'
+    );
+
+-- Edgar Allan Poe
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Edgar Allan',
+        'Poe',
+        '1809-01-19',
+        '1849-10-07',
+        'Edgar Allan Poe was an American writer born on 19 January 1809 and died on 7 October 1849. He is known for his dark and macabre tales. His works often explore themes of madness, death, and the macabre, and he is considered a master of the short story and poetry in the Gothic tradition. ''The Raven'' is one of his most famous poems, and ''The Tell-Tale Heart'' and ''The Fall of the House of Usher'' are among his well-known short stories.'
+    );
+
+-- Mark Twain
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Mark',
+        'Twain',
+        '1835-11-30',
+        '1910-04-21',
+        'Mark Twain (Samuel Clemens) was an American writer born on 30 November 1835 and died on 21 April 1910. He is known for his humour, social commentary, and iconic works such as ''The Adventures of Tom Sawyer'' and ''Adventures of Huckleberry Finn.'''
+    );
+
+-- Emily Brontë
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Emily',
+        'Brontë',
+        '1818-07-30',
+        '1848-12-19',
+        'Emily Brontë was a Victorian novelist born on 30 July 1818 and died on 19 December 1848. She is best known for her only novel, ''Wuthering Heights,'' which is considered a classic of English literature.'
+    );
+
+-- Oscar Wilde
+INSERT INTO
+    authors (
+        first_name,
+        last_name,
+        birth_date,
+        death_date,
+        bio
+    )
+VALUES
+    (
+        'Oscar',
+        'Wilde',
+        '1854-10-16',
+        '1900-11-30',
+        'Oscar Wilde was an Irish playwright, poet, and author born on 16 October 1854 and died on 30 November 1900. Wilde is known for his wit, satirical plays, and flamboyant style. ''The Picture of Dorian Gray'' is his only novel and remains one of his most famous and enduring works. ''The Importance of Being Earnest'' is considered one of the greatest comedies in the English language.'
+    );
---
+++ VictorianAuthors/insert/insert_books.sql
@@ -0,0 +1,571 @@
+-- Sense and Sensibility
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Sense and Sensibility',
+        1,
+        1811,
+        'A novel by Jane Austen about the Dashwood sisters and their romantic pursuits.'
+    );
+
+-- Pride and Prejudice
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Pride and Prejudice',
+        1,
+        1813,
+        'Jane Austen''s classic novel revolving around the romance between Elizabeth Bennet and Mr. Darcy.'
+    );
+
+-- Mansfield Park
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Mansfield Park',
+        1,
+        1814,
+        'The story of Fanny Price, a young woman living with her wealthy relatives.'
+    );
+
+-- Emma
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Emma',
+        1,
+        1815,
+        'Emma Woodhouse, a young woman who meddles in the romantic lives of her friends.'
+    );
+
+-- Northanger Abbey
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Northanger Abbey',
+        1,
+        1818,
+        'A satire of Gothic novels and a coming-of-age story about Catherine Morland.'
+    );
+
+-- Persuasion
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Persuasion',
+        1,
+        1818,
+        'Anne Elliot, a woman who rekindles a past romance with Captain Wentworth.'
+    );
+
+-- Jane Eyre
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Jane Eyre',
+        2,
+        1847,
+        'Charlotte Brontë''s novel follows the life of the titular character, Jane Eyre, as she navigates love, loss, and societal expectations.'
+    );
+
+-- Shirley
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Shirley',
+        2,
+        1849,
+        'Set during the industrial depression of 1811–12, it explores themes of social unrest and the role of women in society.'
+    );
+
+-- Villette
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Villette',
+        2,
+        1853,
+        'A novel by Charlotte Brontë following Lucy Snowe, an Englishwoman who travels to the fictional Belgian town of Villette to teach at a girls'' school.'
+    );
+
+-- Agnes Grey
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Agnes Grey',
+        3,
+        1847,
+        'Anne Brontë''s novel follows Agnes Grey, a governess, as she faces the challenges of working for wealthy families.'
+    );
+
+-- The Tenant of Wildfell Hall
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Tenant of Wildfell Hall',
+        3,
+        1848,
+        'A story by Anne Brontë about a mysterious woman named Helen Graham and her tumultuous marriage.'
+    );
+
+-- The Pickwick Papers
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Pickwick Papers',
+        4,
+        1837,
+        'Charles Dickens''s first novel follows the adventures of Mr. Pickwick and his fellow members of the Pickwick Club.'
+    );
+
+-- Oliver Twist
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Oliver Twist',
+        4,
+        1837,
+        'The story of an orphan boy named Oliver Twist who navigates the harsh realities of 19th-century London.'
+    );
+
+-- Nicholas Nickleby
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Nicholas Nickleby',
+        4,
+        1838,
+        'A young man named Nicholas Nickleby struggles to support his family and encounters various eccentric characters.'
+    );
+
+-- A Christmas Carol
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'A Christmas Carol',
+        4,
+        1843,
+        'A timeless tale of redemption featuring the miserly Ebenezer Scrooge and the ghosts of Christmas Past, Present, and Yet to Come.'
+    );
+
+-- David Copperfield
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'David Copperfield',
+        4,
+        1849,
+        'The life story of David Copperfield, from his childhood to adulthood, with all its trials and tribulations.'
+    );
+
+-- Bleak House
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Bleak House',
+        4,
+        1852,
+        'A complex narrative intertwining the lives of various characters and exploring themes of justice and redemption.'
+    );
+
+-- A Tale of Two Cities
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'A Tale of Two Cities',
+        4,
+        1859,
+        'Set during the French Revolution, the novel contrasts the social unrest in Paris and London.'
+    );
+
+-- Great Expectations
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Great Expectations',
+        4,
+        1860,
+        'The story of Pip, an orphan boy who dreams of becoming a gentleman and encounters various characters along the way.'
+    );
+
+-- Adam Bede
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Adam Bede',
+        5,
+        1859,
+        'George Eliot''s first novel follows the lives of Adam Bede, a carpenter, and Hetty Sorrel, a dairy maid.'
+    );
+
+-- The Mill on the Floss
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Mill on the Floss',
+        5,
+        1860,
+        'A semi-autobiographical novel about the lives of siblings Tom and Maggie Tulliver and their experiences growing up in provincial England.'
+    );
+
+-- Silas Marner
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Silas Marner',
+        5,
+        1861,
+        'The story of Silas Marner, a weaver who finds redemption and purpose after being betrayed by his closest friend.'
+    );
+
+-- Romola
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Romola',
+        5,
+        1862,
+        'Set in Renaissance Florence, the novel follows the life of Romola, a young woman who faces challenges and discovers her own strength.'
+    );
+
+-- Felix Holt, the Radical
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Felix Holt, the Radical',
+        5,
+        1866,
+        'A novel that explores political and social issues in 19th-century England through the story of Felix Holt, a young radical.'
+    );
+
+-- Middlemarch
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Middlemarch',
+        5,
+        1871,
+        'George Eliot''s masterpiece examines the lives of the residents of Middlemarch, a fictional English town, and their interconnected relationships.'
+    );
+
+-- Desperate Remedies
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Desperate Remedies',
+        6,
+        1871,
+        'Thomas Hardy''s first novel follows the romantic entanglements of its characters and explores themes of fate and destiny.'
+    );
+
+-- Under the Greenwood Tree
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Under the Greenwood Tree',
+        6,
+        1872,
+        'A pastoral novel set in the idyllic English countryside, depicting the lives of the villagers and their traditions.'
+    );
+
+-- A Pair of Blue Eyes
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'A Pair of Blue Eyes',
+        6,
+        1873,
+        'A romantic novel about the love triangle between Elfride Swancourt, Stephen Smith, and Henry Knight.'
+    );
+
+-- Far from the Madding Crowd
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Far from the Madding Crowd',
+        6,
+        1874,
+        'The story of Bathsheba Everdene and her relationships with three very different suitors.'
+    );
+
+-- The Return of the Native
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Return of the Native',
+        6,
+        1878,
+        'Set in the fictional Egdon Heath, the novel explores the themes of fate, love, and the conflict between nature and civilization.'
+    );
+
+-- Tess of the d'Urbervilles
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Tess of the d''Urbervilles',
+        6,
+        1891,
+        'The tragic tale of Tess Durbeyfield, a young woman whose life is shaped by the injustices of society and her own choices.'
+    );
+
+-- Jude the Obscure
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Jude the Obscure',
+        6,
+        1895,
+        'A novel about Jude Fawley, a working-class man with intellectual aspirations, and his struggles against societal conventions.'
+    );
+
+-- The Tell-Tale Heart
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Tell-Tale Heart',
+        7,
+        1843,
+        'A short story by Edgar Allan Poe about a narrator who insists on their sanity while describing their murder of an old man with a "vulture eye".'
+    );
+
+-- The Fall of the House of Usher
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Fall of the House of Usher',
+        7,
+        1839,
+        'A Gothic tale about a man who visits his friend Roderick Usher at his decaying mansion, only to witness its terrifying demise.'
+    );
+
+-- The Murders in the Rue Morgue
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Murders in the Rue Morgue',
+        7,
+        1841,
+        'Considered one of the first detective stories, it follows C. Auguste Dupin as he solves a gruesome double murder in Paris.'
+    );
+
+-- The Masque of the Red Death
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Masque of the Red Death',
+        7,
+        1842,
+        'A short story about a prince who throws a lavish masquerade ball while a deadly plague known as the Red Death ravages the country.'
+    );
+
+-- The Pit and the Pendulum
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Pit and the Pendulum',
+        7,
+        1842,
+        'A story about a prisoner of the Spanish Inquisition who faces various tortures, including a swinging pendulum.'
+    );
+
+-- The Raven
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Raven',
+        7,
+        1845,
+        'A narrative poem about a man who is visited by a mysterious raven, leading to his descent into madness and despair.'
+    );
+
+-- The Cask of Amontillado
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Cask of Amontillado',
+        7,
+        1846,
+        'A chilling tale of revenge as Montresor lures Fortunato into his family catacombs to exact a deadly vengeance.'
+    );
+
+-- Annabel Lee
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Annabel Lee',
+        7,
+        1849,
+        'A poem about the love between the speaker and Annabel Lee, ending tragically with her death but continuing in the speaker''s undying love.'
+    );
+
+-- The Celebrated Jumping Frog of Calaveras County
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Celebrated Jumping Frog of Calaveras County',
+        8,
+        1865,
+        'A humorous short story in which the narrator recounts a tale of a jumping frog competition in a California mining camp.'
+    );
+
+-- The Innocents Abroad
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Innocents Abroad',
+        8,
+        1869,
+        'Mark Twain''s travelogue documenting his journey through Europe and the Holy Land with a group of American travelers.'
+    );
+
+-- The Adventures of Tom Sawyer
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Adventures of Tom Sawyer',
+        8,
+        1876,
+        'A classic novel about the mischievous adventures of a young boy named Tom Sawyer along the Mississippi River.'
+    );
+
+-- The Prince and the Pauper
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Prince and the Pauper',
+        8,
+        1881,
+        'A novel about two boys—one a prince, the other a pauper—who switch places and experience life in each other''s shoes.'
+    );
+
+-- Adventures of Huckleberry Finn
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Adventures of Huckleberry Finn',
+        8,
+        1884,
+        'The sequel to "The Adventures of Tom Sawyer," following Huck Finn and Jim, an escaped slave, as they journey down the Mississippi River.'
+    );
+
+-- A Connecticut Yankee in King Arthur's Court
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'A Connecticut Yankee in King Arthur''s Court',
+        8,
+        1889,
+        'A satirical novel in which a 19th-century man named Hank Morgan is transported back in time to King Arthur''s court.'
+    );
+
+-- Wuthering Heights
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Wuthering Heights',
+        9,
+        1847,
+        'Emily Brontë''s novel about the passionate and destructive love between Heathcliff and Catherine Earnshaw.'
+    );
+
+-- The Picture of Dorian Gray
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Picture of Dorian Gray',
+        10,
+        1890,
+        'Oscar Wilde''s novel about a young man named Dorian Gray who remains youthful while a portrait of him ages.'
+    );
+
+-- The Happy Prince and Other Tales
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Happy Prince and Other Tales',
+        10,
+        1888,
+        'A collection of fairy tales and stories written by Oscar Wilde, including "The Happy Prince," "The Nightingale and the Rose," and "The Selfish Giant."'
+    );
+
+-- De Profundis
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'De Profundis',
+        10,
+        1905,
+        'A letter written by Oscar Wilde during his imprisonment, reflecting on his life, spirituality, and the nature of suffering.'
+    );
+
+-- The Importance of Being Earnest
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'The Importance of Being Earnest',
+        10,
+        1895,
+        'A comedic play satirizing Victorian society and its conventions, particularly regarding marriage and morality.'
+    );
+
+-- Salomé
+INSERT INTO
+    books (title, author_id, publication_year, summary)
+VALUES
+    (
+        'Salomé',
+        10,
+        1893,
+        'A one-act play by Oscar Wilde, based on the biblical story of Salomé and her dance of the seven veils.'
+    );
